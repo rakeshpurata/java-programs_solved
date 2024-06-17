ef21053f0c14a7560e228798cefea99b4ef73ced
rakeshpurata <rakeshpurata123@gmail.com>
2024-06-17 14:04:44 +0530

    Create strings.java

---
+++ strings.java
@@ -0,0 +1,13 @@
+class String1
+{
+	public static void main(String[] args) 
+	{
+		String s=("virat kohli");
+		for (int i=0;i<=s.length()-1 ;i++ )
+		{
+			char ch=s.charAt(i);
+			System.out.println(ch);
+		}
+		
+   }
+}
