e894f2fbd9f6e0303cfd382e59189577b39e1c37
rakeshpurata <rakeshpurata123@gmail.com>
2024-06-17 13:54:51 +0530

    Create split_method.java

---
+++ split_method.java
@@ -0,0 +1,14 @@
+class SplitMethod 
+{
+	public static void main(String[] args) 
+	{
+		String S="MRABEFCDABGHKABYZ";	
+		String [] s=S.split("AB");
+		for(int i=0;i<=a.length-1;i++)
+		{		
+
+			
+		System.out.println(a[i]);
+	}
+}
+}
